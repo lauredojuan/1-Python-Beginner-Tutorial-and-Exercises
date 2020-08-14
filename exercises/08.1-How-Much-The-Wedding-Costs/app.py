e2359fe87b53da4c13219ca9ca12163be044b313
Juan L <lauredojuan@gmail.com>
2020-08-14 20:49:31 +0000

    update

--- exercises/08.1-How-Much-The-Wedding-Costs/app.py
+++ exercises/08.1-How-Much-The-Wedding-Costs/app.py
@@ -1,6 +1,12 @@
 user_input = int(input('How many people are coming to your wedding?\n'))
 
-if user_input < 50:
+if user_input < 51:
     price = 4000
+elif user_input < 101:
+    price = 10000
+elif user_input < 201:
+    price = 15000
+else: 
+    price = 20000
 
 print('Your wedding will cost '+str(price)+' dollars')
